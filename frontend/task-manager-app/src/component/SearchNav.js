11fba186bce8a6110e50648f154ecaea5acdbb0b
sathish <sathish@gmail.com>
2024-03-04 22:22:08 +0530

    added project

---
+++ frontend/task-manager-app/src/component/SearchNav.js
@@ -0,0 +1,18 @@
+import "./SearchNav.css";
+import logo from "../pics/EL MAHDI BOUZKOURA.jpg";
+
+
+
+export default function SearchNav(){
+    return (
+        <nav className="search_nav">
+            <i className="fa-solid fa-bars-staggered" onClick={()=>document.querySelector("#small_screen").style.display = "block"}></i> 
+            
+            <div id="search_input_container">
+            <i className="fa-solid fa-magnifying-glass"></i>
+            <input type="search" placeholder="Search" id="search_input"/>
+            </div>
+         
+        </nav>
+    )
+}
